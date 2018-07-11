6e87f110efa4b59546bcd87273efe881c0c8879a
Tara Tran <taratptran@gmail.com>
2018-07-11 16:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+function getUKTime(){
+  return new Date().getHours() + 8
+  
+}
+
+function discountTicket(x){
+  return x*.80
+  
+}
+
+function tweetReply(fanName){
+  return ("Hey " + fanName + ", thanks so much for the support! I really appreciate it!")
+}
